17aa0bbbfda37fe2a5f36cbb7978f746b0a97108
Kseniya Lakhmakova <ksenialahmakova@gmail.com>
2023-10-06 12:02:47 +0200

    first commit

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+import './styles/app.scss';
+import './styles/form.scss';
+import './styles/table.scss';
+import './styles/item_book.scss';
+import './styles/delete_btn.scss';
+import './styles/sorting.scss';
---
+++ src/Form/BookType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Book;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('author')
+            ->add('description', TextareaType::class)
+            ->add('publishedYear')
+            ->add('price')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
